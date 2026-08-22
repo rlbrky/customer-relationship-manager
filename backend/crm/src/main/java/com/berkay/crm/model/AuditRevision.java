fd2f987206f148f0a1a692562dccb9171df24f7b
RLBRKY <erol.berkay.cs@gmail.com>
2026-08-22 17:21:13 +0300

    M11: envers revision entity + listener

--- backend/crm/src/main/java/com/berkay/crm/model/AuditRevision.java
+++ backend/crm/src/main/java/com/berkay/crm/model/AuditRevision.java
@@ -1,4 +1,49 @@
 package com.berkay.crm.model;
 
+import jakarta.persistence.*;
+import org.hibernate.envers.RevisionEntity;
+import org.hibernate.envers.RevisionNumber;
+import org.hibernate.envers.RevisionTimestamp;
+
+@Entity
+@Table(name = "revinfo")
+@RevisionEntity(AuditRevisionListener.class)
 public class AuditRevision {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @RevisionNumber
+    @Column(name = "rev")
+    private int id;
+
+    @RevisionTimestamp
+    @Column(name = "revtstmp")
+    private long timestamp;
+
+    @Column(name = "username", length = 255)
+    private String username;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
--- backend/crm/src/main/java/com/berkay/crm/model/AuditRevisionListener.java
+++ backend/crm/src/main/java/com/berkay/crm/model/AuditRevisionListener.java
@@ -1,4 +1,12 @@
 package com.berkay.crm.model;
 
-public class AuditRevisionListener {
+import com.berkay.crm.security.CurrentUsername;
+import org.hibernate.envers.RevisionListener;
+
+public class AuditRevisionListener implements RevisionListener {
+
+    @Override
+    public void newRevision(Object revisionEntity) {
+        ((AuditRevision) revisionEntity).setUsername(CurrentUsername.get());
+    }
 }
